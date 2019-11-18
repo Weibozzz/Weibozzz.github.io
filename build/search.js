c2b912fd3078e8d7e018666652a2aef4b2f734e4
liuweibo <wb-lwb414922@antfin.com>
2019-11-18 20:07:54 +0800

    chore: 自动化生成搜索

---
+++ build/search.js
@@ -0,0 +1,57 @@
+var fs = require('fs');
+var path = require('path');
+function resolvePath(filename){
+  return path.resolve(__dirname, filename)
+}
+
+//解析需要遍历的文件夹，我这以E盘根目录为例
+var filePath = resolvePath('../docs');
+
+const rootDir = resolvePath('../')
+const filesArr = ["/README"]
+//调用文件遍历方法
+fileDisplay(filePath);
+
+// console.log(filesArr)
+indexStr().then(res => {
+  fs.writeFileSync(resolvePath('../index.html'), res)
+})
+function indexStr () {
+  return new Promise((resolve, reject) => {
+    fs.readFile(resolvePath('../template.html'), (err, data) => {
+      if (err) throw err;
+      const strFiles =
+      resolve(
+        data.toString()
+          .replace("['自定义path']", JSON.stringify(filesArr)
+        )
+      )
+    })
+  });
+}
+/**
+ * 文件遍历方法
+ * @param filePath 需要遍历的文件路径
+ */
+function fileDisplay(filePath){
+  //根据文件路径读取文件，返回文件列表
+  const files = fs.readdirSync(filePath);
+  //遍历读取到的文件列表
+  files.forEach(function(filename){
+    //获取当前文件的绝对路径
+    var filedir = path.join(filePath,filename);
+    //根据文件路径获取文件信息，返回一个fs.Stats对象
+    const stats = fs.statSync(filedir)
+    var isFile = stats.isFile();//是文件
+    var isDir = stats.isDirectory();//是文件夹
+    if(isFile){
+      filesArr.push(
+        filedir.replace(rootDir, '')
+          .replace('.md', '')
+      )
+    }
+    if(isDir){
+      fileDisplay(filedir);//递归，如果是文件夹，就继续遍历该文件夹下面的文件
+    }
+  });
+}
